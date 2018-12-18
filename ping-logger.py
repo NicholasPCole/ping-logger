fbb21c0f0abbfc9876d03d570bbe2cbd261c611e
Nicholas P. Cole <nicholas@nicholaspcole.com>
2018-12-18 15:44:34 -0500

    Refactor InfluxDB code into its own function (resolves #5)
    This better contains any InfluxDB-related logic to a single function,
    whereas previously it was mixed at the top-level and inside a function.
    
    This will also make it easier to add other output formats in the future.

--- ping-logger.py
+++ ping-logger.py
@@ -9,41 +9,53 @@ import time
 import yaml
 
 
-def convert_to_point(line):
-    """Convert an fping output line to InfluxDB line protocol.
+def post_to_influxdb():
+    """Formats fping output and posts it to an InfluxDB server."""
 
-    An example fping output line looks like this:
+    points = []
 
-    example.com : 3.56 1.88 - - - 1.18 1.28 1.39 4.67 1.27
-    """
+    # An example fping output line looks like this:
+    #
+    # example.com : 3.56 1.88 - - - 1.18 1.28 1.39 4.67 1.27
+    for line in fping_output_lines:
+        host = line.split(':')[0].rstrip()
+        responses = line.split(':')[1].lstrip().split(' ')
+        pings = [float(response) for response in responses if response != '-']
 
-    host = line.split(':')[0].rstrip()
-    responses = line.split(':')[1].lstrip().split(' ')
-    pings = [float(response) for response in responses if response != '-']
+        if len(pings) == 0:
+            continue
 
-    if len(pings) == 0:
-        return
+        minimum = min(pings)
+        average = "{0:.2f}".format(statistics.mean(pings))
+        maximum = max(pings)
+        standard_deviation = "{0:.2f}".format(statistics.pstdev(pings))
+        loss = responses.count("-") / config['ping_count']
 
-    minimum = min(pings)
-    average = "{0:.2f}".format(statistics.mean(pings))
-    maximum = max(pings)
-    standard_deviation = "{0:.2f}".format(statistics.pstdev(pings))
-    loss = responses.count("-") / config['ping_count']
+        tags = [
+            'ping',
+            'src=' + config['src_host_name'],
+            'dest=' + host
+        ]
+        fields = [
+            'min=' + str(minimum),
+            'avg=' + str(average),
+            'max=' + str(maximum),
+            'sd=' + str(standard_deviation),
+            'loss=' + str(loss)
+        ]
 
-    tags = [
-        'ping',
-        'src=' + config['src_host_name'],
-        'dest=' + host
-    ]
-    fields = [
-        'min=' + str(minimum),
-        'avg=' + str(average),
-        'max=' + str(maximum),
-        'sd=' + str(standard_deviation),
-        'loss=' + str(loss)
-    ]
+        points.append(','.join(tags) + ' ' + ','.join(fields)
+                      + ' ' + str(timestamp))
 
-    return(','.join(tags) + ' ' + ','.join(fields) + ' ' + str(timestamp))
+    requests.post(
+        config['influxdb_connection']['server']
+        + '/write?db='
+        + config['influxdb_connection']['database']
+        + '&precision=s',
+        auth=(config['influxdb_connection']['username'],
+              config['influxdb_connection']['password']),
+        data='\n'.join(points)
+    )
 
 
 # Load the configuration.
@@ -51,31 +63,13 @@ def convert_to_point(line):
 config = yaml.safe_load(open(os.getenv('HOME')
                              + '/.config/ping-logger/config.yaml'))
 concatenated_hosts = '\n'.join(config['dest_hosts'])
-timestamp = int(time.time())
 
 # Now run the test!
 
+timestamp = int(time.time())
 fping_run = subprocess.run([shutil.which('fping'), '-C',
                            str(config['ping_count']), '-q', '-R'],
                            input=concatenated_hosts, stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT, universal_newlines=True)
 fping_output_lines = fping_run.stdout.splitlines()
-points = []
-
-for line in fping_output_lines:
-    point = convert_to_point(line)
-
-    if point is not None:
-        points.append(point)
-
-# Post the results to InfluxDB.
-
-influxdb_post = requests.post(
-    config['influxdb_connection']['server']
-    + '/write?db='
-    + config['influxdb_connection']['database']
-    + '&precision=s',
-    auth=(config['influxdb_connection']['username'],
-          config['influxdb_connection']['password']),
-    data='\n'.join(points)
-)
+post_to_influxdb()
